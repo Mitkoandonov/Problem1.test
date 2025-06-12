3f3f60fe4c7b0b973ce88e81133a36ddd6b9dbb4
Mitkoandonov <dimitarandonov2009@gmail.com>
2025-06-12 11:01:45 +0300

    Add files via upload

---
+++ Problem.1.test/12.06.25.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem._1.test
+{
+    internal class Class1
+
+
+    {
+        public class BankAccount
+        {
+            private int id;
+            private double balance;
+            public int ID
+            {
+                get { return id; }
+                set { id = value; }
+            }
+            public double Balance
+            {
+                get { return balance; }
+            }
+            public void Deposit(double amount)
+            {
+                balance += amount;
+            }
+            public void Withdraw(double amount)
+            { 
+            if (amount<=amount)
+                {
+                    balance -= amount;
+                }
+                else
+                {
+                    Console.WriteLine(" Nedostupna licnost");
+                }
+            }
+            public override string ToString() 
+            { 
+               return$"ID:{id}, Balance:{balance:F2}";
+            }
+        }
+    }
+}
---
+++ Problem.1.test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Problem._1.test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            
+            
+
+            
+            
+        }
+    }
+}
